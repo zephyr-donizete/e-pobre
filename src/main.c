0222be4ddf09fd76df2c722a9af4bbd3263cff89
DonizeteVida <donizetevida1998@outlook.com>
2025-06-03 22:24:36 +0200

    Add LVGL code and native_sim config

--- src/main.c
+++ src/main.c
@@ -1,13 +1,51 @@
-#include <stdio.h>
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(app, LOG_LEVEL_DBG);
+
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/display.h>
+
+#include <stdio.h>
+#include <string.h>
+
+#include <lvgl.h>
 
 #define SLEEP_TIME_MS 1000
 
 int main(void)
 {
-	while (1) {
-		printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
-		k_msleep(SLEEP_TIME_MS);
+	uint32_t count = 0;
+	char count_str[11] = {0};
+	const struct device *display_dev;
+	lv_obj_t *hello_world_label;
+	lv_obj_t *count_label;
+
+	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+	if (!device_is_ready(display_dev)) {
+		LOG_ERR("Device not ready, aborting test");
+		return 0;
 	}
-	return 0;
+
+	hello_world_label = lv_label_create(lv_scr_act());
+
+	lv_label_set_text(hello_world_label, "Hello world!");
+	lv_obj_align(hello_world_label, LV_ALIGN_CENTER, 0, 0);
+
+	count_label = lv_label_create(lv_scr_act());
+	lv_obj_align(count_label, LV_ALIGN_BOTTOM_MID, 0, 0);
+
+	lv_task_handler();
+	display_blanking_off(display_dev);
+
+	while (1) {
+		if ((count % 100) == 0U) {
+			sprintf(count_str, "%d", count/100U);
+			lv_label_set_text(count_label, count_str);
+		}
+		lv_task_handler();
+		++count;
+		k_sleep(K_MSEC(10));
+		LOG_DBG("While 1");
+	}
 }
